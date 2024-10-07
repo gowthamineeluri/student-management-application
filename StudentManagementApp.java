3ec910f9133337963b73a949605eada080f47ee4
gowthamineeluri <gowthamineeluri123@gmail.com>
2024-10-07 14:20:22 +0530

    Add files via upload

---
+++ StudentManagementApp.java
@@ -0,0 +1,9 @@
+public class StudentManagementApp {
+    public static void main(String[] args){
+        if (args.length > 0) {
+            System.out.println(args[0]);
+        } else {
+            System.out.println("No argument given.");
+        }
+    }
+}
